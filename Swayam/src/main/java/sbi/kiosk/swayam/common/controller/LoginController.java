6a835c509559ba20f09c0c7031989c312f9b6b6e
lenovo <satendra.mishra@mphasis.com>
2020-07-25 19:38:41 +0530

    added anotion

--- Swayam/src/main/java/sbi/kiosk/swayam/common/controller/LoginController.java
+++ Swayam/src/main/java/sbi/kiosk/swayam/common/controller/LoginController.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,6 +45,7 @@ public class LoginController{
 	
 	@SuppressWarnings("deprecation")
 	@GetMapping("authenticateUser")
+	@PostAuthorize("hasPermission('login','READ')")
 	public ModelAndView home(@RequestParam(value="token")String token, HttpSession session,AuditLogger auditLogger) {
 		
 		//logger.info("Inside /authenticateUser?token"+token );
