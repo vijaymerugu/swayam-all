25e671b25602493d88187320c165f504159fb5e2
Sharan <Sharan@LAPTOP-MQCHSJ5R>
2020-10-28 18:26:04 +0530

    PO Genarete no row selected alert

--- Swayam/src/main/webapp/resources/js/purchaseOrder.js
+++ Swayam/src/main/webapp/resources/js/purchaseOrder.js
@@ -166,8 +166,15 @@ var app = angular.module('app', ['ui.grid','ui.grid.pagination','ngAnimate','ui.
 		  
 		    $scope.getRowsData= function () {
 		        var rows = $scope.gridApi.selection.getSelectedRows();
-		        console.log("Inside Get Row Data");
+		        console.log("Inside Get Row Data "+rows);
 		     
+		        if(rows==''){
+		        	
+		        	alert("Please select atleast one row");
+		        }else{
+		        	
+		       
+		        
 		        angular.forEach(rows, function (row, key) {
 		        	//console.log("PoNumber "+row.poId);
 		        	poIdList.push(row.poId);
@@ -199,7 +206,7 @@ var app = angular.module('app', ['ui.grid','ui.grid.pagination','ngAnimate','ui.
 		        });
         	
        
-		        
+		        }
 		    };
 	   
 	   $scope.gridOptions = {
