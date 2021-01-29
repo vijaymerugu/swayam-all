200746465351c875acae2b3ac37678239bf816f6
vmph2382831 <vmph2382831@GSP3SBI3A012.CORP.AD.SBI>
2021-01-29 13:32:16 +0530

    commit

--- Swayam/src/main/java/sbi/kiosk/swayam/common/controller/LoginController.java
+++ Swayam/src/main/java/sbi/kiosk/swayam/common/controller/LoginController.java
@@ -5,12 +5,20 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.security.GeneralSecurityException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
@@ -61,6 +69,48 @@ public class LoginController{
 	CommonUrlConfigRepository commonUrlConfigRepo;
 	//@Autowired
 	//private JwtUtil jwtTokenUtil;
+	
+	
+	
+	
+	 static {
+	        // this part is needed cause Lebocoin has invalid SSL certificate, that cannot be normally processed by Java
+	        TrustManager[] trustAllCertificates = new TrustManager[]{
+	                new X509TrustManager() {
+	                    @Override
+	                    public X509Certificate[] getAcceptedIssuers() {
+	                        return null; // Not relevant.
+	                    }
+
+	                    @Override
+	                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
+	                        // Do nothing. Just allow them all.
+	                    }
+
+	                    @Override
+	                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
+	                        // Do nothing. Just allow them all.
+	                    }
+	                }
+	        };
+
+	        HostnameVerifier trustAllHostnames = new HostnameVerifier() {
+	            @Override
+	            public boolean verify(String hostname, SSLSession session) {
+	                return true; // Just allow them all.
+	            }
+	        };
+
+	        try {
+	            System.setProperty("jsse.enableSNIExtension", "false");
+	            SSLContext sc = SSLContext.getInstance("SSL");
+	            sc.init(null, trustAllCertificates, new SecureRandom());
+	            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+	            HttpsURLConnection.setDefaultHostnameVerifier(trustAllHostnames);
+	        } catch (GeneralSecurityException e) {
+	            throw new ExceptionInInitializerError(e);
+	        }
+	    }
 	
 	// comments 27-Jan-2021 this is method for jwt token
 	
